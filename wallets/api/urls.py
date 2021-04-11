463a96d9c31402e172b17fa433d57b9c4abcbd40
Aleksandr Zagoskin <zaleksandrne@gmail.com>
2021-04-11 19:08:52 +0300

    api documintation

--- wallets/api/urls.py
+++ wallets/api/urls.py
@@ -1,13 +1,34 @@
 from django.urls import include, path
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from .views import WalletViewSet, TransactionViewSet
 
 router = DefaultRouter()
 
-#router.register('transactions', TransactionViewSet)
 router.register('wallets', WalletViewSet)
-router.register(r'wallets/(?P<id>\d+)/transactions', TransactionViewSet, basename = 'transactions')
+router.register(r'wallets/(?P<id>\d+)/transactions',
+                TransactionViewSet,
+                basename='transactions'
+                )
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
+
 urlpatterns = [
-    path('', include(router.urls)),
+    path('v1/', include(router.urls)),
+    path('v1/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
